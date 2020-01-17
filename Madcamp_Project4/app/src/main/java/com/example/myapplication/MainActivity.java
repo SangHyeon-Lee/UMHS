e9a49e2d03f3350b913db15b48cf140d13fc3505
leesecre <59246292+leesecre@users.noreply.github.com>
2020-01-17 22:37:32 +0900

    facebooklogin

--- Madcamp_Project4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Madcamp_Project4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.myapplication.facebooklogin.LoginActivity;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
+    private TextView emailTextView;
+    private TextView uidTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        nameTextView = (TextView) findViewById(R.id.nameTextView);
+//        emailTextView = (TextView) findViewById(R.id.emailTextView);
+//        uidTextView = (TextView) findViewById(R.id.uidTextView);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+//            String name = user.getDisplayName();
+//            String email = user.getEmail();
+//            Uri photoUrl = user.getPhotoUrl();
+//            String uid = user.getUid();
+//
+//            nameTextView.setText(name);
+//            emailTextView.setText(email);
+//            uidTextView.setText(uid);
+        } else {
+            goLoginScreen();
+        }
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
     }
 }
