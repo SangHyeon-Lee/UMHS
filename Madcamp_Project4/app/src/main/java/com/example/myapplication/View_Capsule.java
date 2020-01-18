c7b7b7b30a16c1420bb2d9aa37afd3d48e08a4fe
Sanghyeon <lsh1107@kaist.ac.kr>
2020-01-19 00:02:26 +0900

    compass

--- Madcamp_Project4/app/src/main/java/com/example/myapplication/View_Capsule.java
+++ Madcamp_Project4/app/src/main/java/com/example/myapplication/View_Capsule.java
@@ -4,15 +4,41 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 
-public class View_Capsule extends Fragment {
+import static android.content.Context.SENSOR_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+public class View_Capsule extends Fragment implements SensorEventListener {
+
+    private ImageView compass;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+    private float mCurrentDegree = 0f;
+
 
     public static View_Capsule newInstance() {
         View_Capsule fragmentFirst = new View_Capsule();
@@ -41,6 +67,57 @@ public class View_Capsule extends Fragment {
                 startActivity(intent);
             }
         });
+
+        mSensorManager = (SensorManager) this.getContext().getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        compass = (ImageView) view.findViewById(R.id.compass);
+
         return view;
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor==mAccelerometer){
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0 , event.values.length);
+            mLastAccelerometerSet = true;
+        }else if(event.sensor == mMagnetometer){
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0 , event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastMagnetometerSet && mLastAccelerometerSet){
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            float azimuthinDegress = (int) (Math.toDegrees(SensorManager.getOrientation(mR, mOrientation)[0])+ 360)%360;
+            RotateAnimation ra = new RotateAnimation(
+                    mCurrentDegree,
+                    -azimuthinDegress,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f
+            );
+            ra.setDuration(250);
+            ra.setFillAfter(true);
+            compass.startAnimation(ra);
+            mCurrentDegree = -azimuthinDegress;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
